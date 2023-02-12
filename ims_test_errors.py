173f841566a36f8f604929dca59912b4febe1539
mahmutovAR <92520161+mahmutovAR@users.noreply.github.com>
2023-02-12 23:04:11 +0400

    Delete ims_test_errors.py

--- ims_test_errors.py
+++
@@ -1,9 +0,0 @@
-class TestFailedError(Exception):
-    __slots__ = ['__test_name']
-
-    def __init__(self, test_name):
-        self.__test_name = test_name
-        self.__description = f'Error! Test {test_name} failed.'
-
-    def __str__(self):
-        return f'{self.__description}'
